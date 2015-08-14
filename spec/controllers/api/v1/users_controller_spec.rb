6a116baccd6ef8bce0ce7f5c3661f2a7c9f49fea
Patrick <pcofilada@gmail.com>
2015-08-14 22:56:59 +0800

    Fixes users controller spec

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     end
 
     it "returns the information about a reporter on a hash" do
-      user_response = JSON.parse(response.body, sybmolize_names: true)
-      expect(user_response['email']).to eql @user.email
+      user_response = json_response[:user]
+      expect(user_response[:email]).to eql @user.email
     end
 
     it { should respond_with 200 }
@@ -64,7 +64,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
 
       it "renders the json representation for the updated user" do
-        user_response = json_response
+        user_response = json_response[:user]
         expect(user_response[:email]).to eql("newemail@example.com")
       end
 
